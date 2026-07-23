2e944ffa4be924ec47f961175f5265224dfde275
MyeongGwan Son <81242244+internalforces@users.noreply.github.com>
2026-07-23 10:31:44 +0900

    fix(android): avoid duplicate FMOD jniLib copies

--- scripts/install-fmod-android.sh
+++ scripts/install-fmod-android.sh
@@ -17,17 +17,12 @@ for required in   "$API_ROOT/core/inc/fmod.hpp"   "$API_ROOT/core/lib/fmod.jar" 
   fi
 done
 
-mkdir -p "$DEST_ROOT/vendor" "$DEST_ROOT/libs" "$DEST_ROOT/jniLibs" "$DEST_ROOT/assets"
+mkdir -p "$DEST_ROOT/vendor" "$DEST_ROOT/libs" "$DEST_ROOT/assets"
 cp -R "$API_ROOT" "$DEST_ROOT/vendor/"
 cp "$API_ROOT/core/lib/fmod.jar" "$DEST_ROOT/libs/"
 cp "$API_ROOT/studio/examples/media/Master.bank" "$DEST_ROOT/assets/"
 cp "$API_ROOT/studio/examples/media/Master.strings.bank" "$DEST_ROOT/assets/"
 cp "$API_ROOT/studio/examples/media/Vehicles.bank" "$DEST_ROOT/assets/"
 
-for abi in arm64-v8a armeabi-v7a x86 x86_64; do
-  mkdir -p "$DEST_ROOT/jniLibs/$abi"
-  cp "$API_ROOT/core/lib/$abi/libfmod.so" "$DEST_ROOT/jniLibs/$abi/"
-  cp "$API_ROOT/studio/lib/$abi/libfmodstudio.so" "$DEST_ROOT/jniLibs/$abi/"
-done
 
 echo "FMOD Engine 2.03.14 Android files installed locally."
